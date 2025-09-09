7aab745f82b8dc9e7f6ec8590fe115ca7cb14a93
Preston Evans <preston.b.evans@gmail.com>
2025-09-09 16:39:52 +0200

    Fix warnings from new rustc versio

--- src/lib.rs
+++ src/lib.rs
@@ -398,7 +398,7 @@ impl DB {
         &self,
         opts: ReadOptions,
         direction: ScanDirection,
-    ) -> anyhow::Result<SchemaIterator<S>> {
+    ) -> anyhow::Result<SchemaIterator<'_, S>> {
         assert!(
             !S::SHOULD_CACHE,
             "Caching is incompatible with iterators! Cannot iterate over {}",
@@ -413,7 +413,7 @@ impl DB {
 
     /// Returns a forward [`SchemaIterator`] on a certain schema with the default read options.
     #[tracing::instrument(skip_all, level = "error")]
-    pub fn iter<S: Schema>(&self) -> anyhow::Result<SchemaIterator<S>> {
+    pub fn iter<S: Schema>(&self) -> anyhow::Result<SchemaIterator<'_, S>> {
         self.iter_with_direction::<S>(Default::default(), ScanDirection::Forward)
     }
 
@@ -422,7 +422,7 @@ impl DB {
         &self,
         from: &impl SeekKeyEncoder<S>,
         to: &impl SeekKeyEncoder<S>,
-    ) -> anyhow::Result<SchemaIterator<S>> {
+    ) -> anyhow::Result<SchemaIterator<'_, S>> {
         with_error_logging(
             || {
                 let mut opts = ReadOptions::default();
@@ -438,7 +438,7 @@ impl DB {
     pub(crate) fn raw_iter<S: Schema>(
         &self,
         direction: ScanDirection,
-    ) -> anyhow::Result<RawDbIter> {
+    ) -> anyhow::Result<RawDbIter<'_>> {
         assert!(
             !S::SHOULD_CACHE,
             "Caching is incompatible with iterators! Cannot iterate over {}",
@@ -453,7 +453,7 @@ impl DB {
         &self,
         range: impl std::ops::RangeBounds<SchemaKey>,
         direction: ScanDirection,
-    ) -> anyhow::Result<RawDbIter> {
+    ) -> anyhow::Result<RawDbIter<'_>> {
         assert!(
             !S::SHOULD_CACHE,
             "Caching is incompatible with iterators! Cannot iterate over {}",
@@ -471,7 +471,7 @@ impl DB {
     pub fn iter_with_opts<S: Schema>(
         &self,
         opts: ReadOptions,
-    ) -> anyhow::Result<SchemaIterator<S>> {
+    ) -> anyhow::Result<SchemaIterator<'_, S>> {
         self.iter_with_direction::<S>(opts, ScanDirection::Forward)
     }
 
